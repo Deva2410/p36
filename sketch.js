4dbbd47be54d4ba53e3bce7d890ccfd2976295be
Deva2410 <74659088+Deva2410@users.noreply.github.com>
2020-12-30 18:40:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+//Create variables here
+var dog, happydog;
+var database;
+var foodS;
+var foodStock;
+var lastFed;
+var DogImg1,DogImg2,MilkImg;
+function preload()
+{
+  DogImg1 = loadImage("dogImg.png");
+  DogImg2 = loadImage("dogImg1.png");
+  MilkImg = loadImage("Milk.png");
+}
+
+function setup() {
+  createCanvas(800, 550);
+
+  database = firebase.database();
+
+  foodObj = new Food();
+
+  foodStock = database.ref("Food");
+  foodStock.on("value",readStock);
+  
+  Dog = createSprite(475,250,50,50);
+  Dog.addImage(DogImg1);
+  Dog.scale = 0.2;
+
+  feed = createButton("Feed Drago");
+  feed.position(650,200);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(650,250);
+  addFood.mousePressed(addFoods);
+
+}
+
+
+function draw() {
+  background(31, 196, 118);
+  foodObj.display();
+
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+  
+  greetDrago = createButton("Greet Drago!!")
+  greetDrago.position(650,300);
+  //greetDrago.mousePressed(text("Hi Drago!! How are you",400,300));
+  greetDrago.mousePressed(Message);
+ 
+  drawSprites();
+
+  //fill(238, 241, 18);
+  //noStroke();
+  //textSize(30);
+  //text("Food Remaining: " + foodS,50,265);
+  //add styles here
+
+
+  
+
+  //Creating Rectangles Around The Buttons
+  fill(236, 130, 23);
+  stroke(236, 130, 23);
+  rect(608,160,100,33);
+  rect(608,210,88,33);
+  rect(609,260,107,33);
+
+
+  fill(206, 0, 9);
+  noStroke();
+  textSize(30);
+  if(lastFed>=12){
+    text("Last Fed : "+ lastFed%12 + " PM", 380,140);
+   }else if(lastFed==0){
+     text("Last Fed : 12 AM",380,140);
+   }else{
+     text("Last Fed : "+ lastFed + " AM", 380,140);
+   }
+
+}
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+//function to update food stock and last fed time
+function feedDog(){
+  Dog.addImage(DogImg2);
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  Dog.addImage(DogImg1);
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+function Message(){
+  mssg = createElement("h3");
+  mssg.html("Hi Drago! How are you?");
+  mssg.position(600,350);
+}
+  //rgb(7, 46, 129)
